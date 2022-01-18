df94deb0df17e72de2c5bb160762181a9004ff8c
MarkHolleran <mark.holleran@rutgers.edu>
2022-01-18 13:16:01 -0500

    test

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	// write your code here
 
         System.out.println("test");
+
+        int a = 5;
+        int b =4;
+        int result = a+b;
+        System.out.println(result);
     }
 }
